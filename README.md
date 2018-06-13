c0d3c5b9fe63b778dbc78e26f277793a99e443af
Carlton Joseph <carlton.joseph@gmail.com>
2018-06-13 11:15:44 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GraphQL and Mongo
+
+The code in this repository is based on the
+[Build GraphQL APIs with Node.js on MongoDB](https://egghead.io/courses/build-graphql-apis-with-node-js-on-mongodb)
+video series.
+
+The first commit can be used as a minimal GraphQL server setup (aka boiler plate).
---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import graphqlHTTP from "express-graphql";
+
+import schema from "./schema";
+
+const app = express();
+
+const PORT = process.env.PORT || "3000";
+
+app.get("/", (req, res) => {
+  return res.json({ msg: "hi" });
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
